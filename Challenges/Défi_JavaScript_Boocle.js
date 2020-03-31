dd7f551ce88455dd0a3a46e9d994976126546f73
iamsoltan <61824434+iamsoltan@users.noreply.github.com>
2020-03-31 21:14:14 +0100

    Update Défi_JavaScript_Boocle.js

--- Challenges/Défi_JavaScript_Boocle.js
+++ Challenges/Défi_JavaScript_Boocle.js
@@ -73,3 +73,40 @@ console.log(unlucky(["13",13,null,125,"hay"]));
 function transform(array) { return array.map( x => (x % 2 == 0) ? x-=1 : x+=1 )};
 console.log(transform([1, 2, 3, 4, 5]));
 /************************************************************** */
+function filterArray(array) { return array.filter( x => (typeof(x) != "string"))};
+console.log(filterArray([1, 2, 3, 4, 5,null, "hhh"]));
+/************************************************************** */
+function societyName(array) { return (array.map( x => ( x[0]))).sort().join("")};
+console.log(societyName(["Saief", "Mouna", "Sirine"]));
+/************************************************************** */
+function findLargestNums(array) { return  array.map( x => Math.max(...x))  };
+console.log(findLargestNums([[9, 2, 3, 1], [20, 70, 40, 90], [4, 2, 3]]));
+/************************************************************** */
+function shortToLong(array) { return array.sort((a, b) => a.length - b.length) };
+console.log(shortToLong(["Google", "Apple", "Microsoft"]));
+/************************************************************** */
+function toArray(array) { return (array+"").split("") };
+console.log(toArray(488));
+/************************************************************** */
+function toNumber(array) { return array.join("") };
+console.log(toNumber([4, 8, 8]));
+/************************************************************** */
+function spelling(array) { let y = ""; return   (array.split("")).map(  x => y+= x ) };
+console.log(spelling("abc"));
+/************************************************************** */
+function toArray(array) { return Object.entries(array)};
+console.log(toArray({ a: 1, b: 2 }));
+/************************************************************** */
+function getSalaries(array) {let y=null;let array2 = (array.map( (x) => y = y + x.salary)); return array2[array2.length-1]};
+console.log(getSalaries( [
+    { name: "Amira", age: 21, salary: 23000 },
+    { name: "Takwa",  age: 32, salary: 40000 },
+    { name: "Manel",  age: 16, salary: 2700 } ] ));
+/************************************************************** */
+var h= [1, 2, 3];
+function numOfSubbarrays(array) { return (array.filter( x => (x.constructor == h.constructor))).length};
+console.log(numOfSubbarrays(["chaima", "Soltane", [1, 2, 3], [1, 2, 3], [1, 2, 3], { name: "Manel",  age: 16, salary: 2700 }]));
+/************************************************************** */
+function NoNamesPlease(array) { return (array.map( x => (x.name) ))};
+console.log(NoNamesPlease([{ name: "Ahmed" },{ name: "Achref" },{ name: "Ayoub" }]));
+/************************************************************** */
